341446a55bd7eab0a480c091334bc979d1644c2a
Fredrik Norgren <fredrik.norgren@gmail.com>
2025-06-18 14:16:08 +0200

    Removed Class1.cs from Tournament.Core and fixed a bug in TournamentDetails class

--- Tournament.Data/Entities/TournamentDetails.cs
+++ Tournament.Data/Entities/TournamentDetails.cs
@@ -8,8 +8,9 @@ namespace Tournament.Data.Entities
 {
     public class TournamentDetails
     {
-        public int Id { get; set; } 
+        public int Id { get; set; }
         public string Title { get; set; }
         public DateTime StartDate { get; set; }
-        public ICollection<Game> Games { get; set; } 
+        public ICollection<Game> Games { get; set; }
+    }
 }
