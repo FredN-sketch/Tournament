96abc14bf16c3742f03716bc5f600d3b1fe872fb
Fredrik Norgren <fredrik.norgren@gmail.com>
2025-06-18 13:53:41 +0200

    First commit 1-9

--- Tournament.Data/Entities/TournamentDetails.cs
+++ Tournament.Data/Entities/TournamentDetails.cs
@@ -8,7 +8,7 @@ namespace Tournament.Data.Entities
 {
     public class TournamentDetails
     {
-        public int Id { get; set; }
+        public int Id { get; set; } 
         public string Title { get; set; }
         public DateTime StartDate { get; set; }
         public ICollection<Game> Games { get; set; } 
